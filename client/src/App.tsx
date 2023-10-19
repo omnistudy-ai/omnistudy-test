691443ea78a1ec0a70123146472b22fe8a56903c
grude013 <grude013@umn.edu>
2023-10-18 21:08:28 -0500

    added some new text to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       OmniStudy Platform - Automatic Deploy From Branch Jamison
+
+      <h1>AI Learning</h1>
+      <h1>Transcriptions</h1>
+      <h1>What else folks?</h1>
     </div>
   );
 }
