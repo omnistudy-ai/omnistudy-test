1d76049140170d1f3a1108d366c5b39b3ffa1819
noahschlorf <noah.schlorf@gmail.com>
2023-10-29 20:09:12 -0500

    About pg and working register

--- client/src/tools/firebase.ts
+++ client/src/tools/firebase.ts
@@ -10,13 +10,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-console.log('Firebase API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
-console.log('Firebase Auth Domain:', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-console.log('Firebase Project ID:', process.env.REACT_APP_FIREBASE_PROJECT_ID);
-console.log('Firebase Storage Bucket:', process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-console.log('Firebase Messaging Sender ID:', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
-console.log('Firebase App ID:', process.env.REACT_APP_FIREBASE_APP_ID);
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
