ee999f90f9c609d416bc55f497d720b163238648
noahschlorf <noah.schlorf@gmail.com>
2023-11-24 13:41:15 -0600

    Register w @ symbol in email, 10 digit phone number, and 6 digit password now required

---
+++ client/src/app/components/main/Dashboard 2.tsx
@@ -0,0 +1,29 @@
+import "./Dashboard.css";
+import CourseCard from "./Cards/CourseCard"
+import TaskProgress from "./Cards/TaskProgress";
+import { StudyTimeTracker } from "./Cards/StudyTimeTracker";
+import { GradesBar } from "./Cards/GradesBar";
+import { userGrades } from './Cards/GradesData';
+
+
+
+
+export default function Dashboard() {
+    return (
+        <div className="home-container">
+            <h1>Welcome Back!</h1>
+            <div className="card__container">
+            <CourseCard courseName="Biology 101" timeSpent={5} timeIncreased={true} />
+            <TaskProgress progressBar={75.5} />
+            <StudyTimeTracker currentHours={12} goalHours={18} />
+            
+
+            </div>
+            {/* Wont let me import chartJs so I used a placeholder library till I fix */}
+            {/* <div className="GradesBar">
+            <GradesBar grades={userGrades} />
+            </div> */}
+        </div>
+       
+    );
+}
