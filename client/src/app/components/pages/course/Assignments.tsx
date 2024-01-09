9838bacfa76c95d713438c2c2a073d00b6b35870
moha1747 <mohamud.abbaas14@gmail.com>
2024-01-09 10:53:49 -0600

    add assignment page

--- client/src/app/components/pages/course/Assignments.tsx
+++ client/src/app/components/pages/course/Assignments.tsx
@@ -1,7 +1,56 @@
+import { Avatar, CardContent, Stack, SvgIcon, Typography, Grid } from '@mui/material';
+import Card from '../../../../web/components/UI/Card';
+import './Assignments.css';
+
+interface AssignmentsCard {
+    courseName: string;
+    assignmentName: string;
+    dueDate: Date;
+}
+//sample date format
+// Dummy data for assignments
+const assignments = Array(5).fill({ assignmentName: "Assignment Name", dueDate: "Due Date" });
+
+
 export default function Assignments() {
-    return(
-        <div>
-            App Side - Assignments
+    return (
+        <div className='main-container'>
+            {/* Course Name */}
+            <h1>Course Name</h1>
+            
+            <Card className='assignCard'>
+                <CardContent>
+                    {assignments.map((assignment, index) => (
+                        <div key={index}>
+                            <Typography variant="body1" className="text-padding text-margin">
+                                {assignment.assignmentName}
+                            </Typography>
+                            <Typography variant="body2" className="text-padding text-margin">
+                                {assignment.dueDate}
+                            </Typography>
+                        </div>
+                    ))}
+                </CardContent>
+            </Card>
+          
+
+            {/* Second Card */}
+            <Card className='notesCard'>
+                <CardContent>
+                    <Typography variant="h6" className="text-padding text-margin">
+                           {/* Notes */}
+                           Course Name
+                           
+                    </Typography>
+                    <Typography variant="body1" className="text-padding text-margin">
+                        {/* {course2.assignmentName} */}
+                        </Typography>
+                    <Typography variant="body2" className="text-padding text-margin">
+                        {/* {course2.dueDate} */}
+                        </Typography>
+                </CardContent>
+            </Card>
         </div>
     );
+    
 }
